999e72c16c69ccdfedf70267fd6837913725185b
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2025-06-15 23:41:32 +0530

    auth middleware done

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+export const authenticate = async (req, res, next) => {
+	try {
+		const token =
+			req.cookies?.token ||
+			req.body?.token ||
+			req.header('Authorization')?.replace('Bearer ', '').trim();
+
+		if (!token) {
+			return res.status(401).json({
+				success: false,
+				message: 'Auth token missing!',
+			});
+		}
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.userId = decoded.userId;
+		next();
+	} catch (error) {
+		return res.status(401).json({
+			success: false,
+			message: 'Invalid or expired token',
+			error: error.message,
+		});
+	}
+};
